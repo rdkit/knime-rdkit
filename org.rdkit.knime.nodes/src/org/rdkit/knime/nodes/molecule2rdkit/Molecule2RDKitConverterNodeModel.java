e4b400f7d042fcb9d9b733b16b7e5a9851f5507d
unknown <unknown@knime.com>
2010-11-04 19:23:20 +0000

    - Add SDF to the possible input types  - Update node description  - Update icon

---
+++ org.rdkit.knime.nodes/src/org/rdkit/knime/nodes/molecule2rdkit/Molecule2RDKitConverterNodeModel.java
@@ -0,0 +1,303 @@
+/*
+ * ------------------------------------------------------------------
+ * This source code, its documentation and all appendant files
+ * are protected by copyright law. All rights reserved.
+ *
+ * Copyright (C) 2010
+ * Novartis Institutes for BioMedical Research
+ *
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ */
+package org.rdkit.knime.nodes.molecule2rdkit;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.RDKit.RDKFuncs;
+import org.RDKit.ROMol;
+import org.knime.chem.types.SdfValue;
+import org.knime.chem.types.SmilesValue;
+import org.knime.core.data.DataCell;
+import org.knime.core.data.DataColumnSpec;
+import org.knime.core.data.DataColumnSpecCreator;
+import org.knime.core.data.DataRow;
+import org.knime.core.data.DataTableSpec;
+import org.knime.core.data.DataType;
+import org.knime.core.data.StringValue;
+import org.knime.core.data.container.ColumnRearranger;
+import org.knime.core.data.container.SingleCellFactory;
+import org.knime.core.node.BufferedDataTable;
+import org.knime.core.node.CanceledExecutionException;
+import org.knime.core.node.ExecutionContext;
+import org.knime.core.node.ExecutionMonitor;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.NodeLogger;
+import org.knime.core.node.NodeModel;
+import org.knime.core.node.NodeSettingsRO;
+import org.knime.core.node.NodeSettingsWO;
+import org.knime.core.node.defaultnodesettings.SettingsModelBoolean;
+import org.knime.core.node.defaultnodesettings.SettingsModelString;
+import org.rdkit.knime.RDKitTypesPluginActivator;
+import org.rdkit.knime.types.RDKitMolCellFactory;
+
+/**
+ *
+ * @author Greg Landrum
+ */
+public class Molecule2RDKitConverterNodeModel extends NodeModel {
+
+    private final SettingsModelString m_first =
+            Molecule2RDKitConverterNodeDialogPane.createFirstColumnModel();
+
+    private final SettingsModelString m_concate =
+            Molecule2RDKitConverterNodeDialogPane.createNewColumnModel();
+
+    private final SettingsModelBoolean m_removeSourceCols =
+            Molecule2RDKitConverterNodeDialogPane.createBooleanModel();
+
+    private static final NodeLogger LOGGER = NodeLogger
+            .getLogger(Molecule2RDKitConverterNodeModel.class);
+
+    /**
+     * Temporarily used during execution to track the number of rows
+     * with parsing error.
+     */
+    private int m_parseErrorCount;
+
+    /**
+     * Create new node model with one data in- and one outport.
+     */
+    Molecule2RDKitConverterNodeModel() {
+        super(1, 1);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected DataTableSpec[] configure(final DataTableSpec[] inSpecs)
+            throws InvalidSettingsException {
+        // check whether native RDKit library has been loaded successfully
+        RDKitTypesPluginActivator.checkErrorState();
+
+        if (null == m_first.getStringValue()) {
+            List<String> compatibleCols = new ArrayList<String>();
+            for (DataColumnSpec c : inSpecs[0]) {
+                if (c.getType().isCompatible(SmilesValue.class)
+                        || c.getType().isCompatible(SdfValue.class)) {
+                    compatibleCols.add(c.getName());
+                }
+            }
+            if (compatibleCols.size() == 1) {
+                // auto-configure
+                m_first.setStringValue(compatibleCols.get(0));
+            } else if (compatibleCols.size() > 1) {
+                // auto-guessing
+                m_first.setStringValue(compatibleCols.get(0));
+                setWarningMessage("Auto guessing: using column \""
+                        + compatibleCols.get(0) + "\".");
+            } else {
+                throw new InvalidSettingsException("Neither Smiles nor SDF "
+                        + "compatible "
+                        + "column in input table.");
+            }
+        }
+        if (null == m_concate.getStringValue()) {
+            if (null != m_first.getStringValue()) {
+                // auto-configure
+                String newName = DataTableSpec.getUniqueColumnName(inSpecs[0],
+                        m_first.getStringValue() + " (RDKit Mol)");
+                m_concate.setStringValue(newName);
+            } else {
+                m_concate.setStringValue("RDKit Molecule");
+            }
+        }
+        ColumnRearranger rearranger = createColumnRearranger(inSpecs[0]);
+        return new DataTableSpec[]{rearranger.createSpec()};
+    }
+
+    private int[] findColumnIndices(final DataTableSpec spec)
+            throws InvalidSettingsException {
+        String first = m_first.getStringValue();
+        if (first == null) {
+            throw new InvalidSettingsException("Not configured yet");
+        }
+        int firstIndex = spec.findColumnIndex(first);
+        if (firstIndex < 0) {
+            throw new InvalidSettingsException(
+                    "No such column in input table: " + first);
+        }
+        DataType firstType = spec.getColumnSpec(firstIndex).getType();
+        if (!firstType.isCompatible(SmilesValue.class)
+                || !firstType.isCompatible(SdfValue.class)) {
+            throw new InvalidSettingsException("Column '" + first
+                    + "' does not contain Smiles.");
+        }
+        return new int[]{firstIndex};
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected BufferedDataTable[] execute(final BufferedDataTable[] inData,
+            final ExecutionContext exec) throws Exception {
+        DataTableSpec inSpec = inData[0].getDataTableSpec();
+        ColumnRearranger rearranger = createColumnRearranger(inSpec);
+        m_parseErrorCount = 0;
+        BufferedDataTable outTable =
+                exec.createColumnRearrangeTable(inData[0], rearranger, exec);
+        if (m_parseErrorCount > 0) {
+            setWarningMessage("Error parsing Smiles for " + m_parseErrorCount
+                    + " rows.");
+        }
+        return new BufferedDataTable[]{outTable};
+    }
+
+    private ColumnRearranger createColumnRearranger(final DataTableSpec spec)
+            throws InvalidSettingsException {
+        // check user settings against input spec here
+        final int[] indices = findColumnIndices(spec);
+        String inputCol = m_first.getStringValue();
+        String newName = m_concate.getStringValue();
+        if ((spec.containsName(newName) && !newName.equals(inputCol))
+              ||  (spec.containsName(newName) && newName.equals(inputCol)
+              && !m_removeSourceCols.getBooleanValue())) {
+            throw new InvalidSettingsException("Cannot create column "
+                    + newName + "since it is already in the input.");
+        }
+        ColumnRearranger result = new ColumnRearranger(spec);
+        DataColumnSpecCreator appendSpec =
+                new DataColumnSpecCreator(newName, RDKitMolCellFactory.TYPE);
+        final boolean smilesInput = spec.getColumnSpec(indices[0]).getType().
+            isCompatible(SmilesValue.class);
+        result.append(new SingleCellFactory(appendSpec.createSpec()) {
+            @Override
+            public DataCell getCell(final DataRow row) {
+                DataCell firstCell = row.getCell(indices[0]);
+                if (firstCell.isMissing()) {
+                    return DataType.getMissingCell();
+                }
+                String value = ((StringValue)firstCell).toString();
+                ROMol mol = null;
+                if (smilesInput) {
+                    mol = RDKFuncs.MolFromSmiles(value);
+                    if (mol == null) {
+                        LOGGER.debug("Error parsing smiles "
+                                + "while processing row: " + row.getKey());
+                        m_parseErrorCount ++;
+                        return DataType.getMissingCell();
+                    }
+
+                } else {
+                    mol = RDKFuncs.MolFromMolBlock(value);
+                }
+                return RDKitMolCellFactory.createRDKitMolCell(mol);
+            }
+        });
+        if (m_removeSourceCols.getBooleanValue()) {
+            result.remove(indices);
+        }
+        return result;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void reset() {
+        // nothing to reset
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void loadInternals(final File nodeInternDir,
+            final ExecutionMonitor exec) throws IOException,
+            CanceledExecutionException {
+        // node does not have internals
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void saveInternals(final File nodeInternDir,
+            final ExecutionMonitor exec) throws IOException,
+            CanceledExecutionException {
+        // node does not have internals
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void loadValidatedSettingsFrom(final NodeSettingsRO settings)
+            throws InvalidSettingsException {
+        m_first.loadSettingsFrom(settings);
+        m_concate.loadSettingsFrom(settings);
+        m_removeSourceCols.loadSettingsFrom(settings);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void saveSettingsTo(final NodeSettingsWO settings) {
+        m_first.saveSettingsTo(settings);
+        m_concate.saveSettingsTo(settings);
+        m_removeSourceCols.saveSettingsTo(settings);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void validateSettings(final NodeSettingsRO settings)
+            throws InvalidSettingsException {
+        m_first.validateSettings(settings);
+        m_concate.validateSettings(settings);
+        m_removeSourceCols.validateSettings(settings);
+    }
+}

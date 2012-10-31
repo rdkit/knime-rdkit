c765740eec75d1336be4dbe6888a7a78e0212ace
Manuel Schwarze <manuel.schwarze@novartis.com>
2012-10-31 11:41:04 +0000

    Copyright corrected.

--- org.rdkit.knime.nodes/src-experimental/org/rdkit/knime/nodes/quickform/multiselection/MultiSelectionInputQuickFormValueInConfiguration.java
+++ org.rdkit.knime.nodes/src-experimental/org/rdkit/knime/nodes/quickform/multiselection/MultiSelectionInputQuickFormValueInConfiguration.java
@@ -3,9 +3,8 @@
  * This source code, its documentation and all appendant files
  * are protected by copyright law. All rights reserved.
  *
- * Copyright (C) 2010
+ * Copyright (C) 2012
  * Novartis Institutes for BioMedical Research
- *
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License, Version 3, as

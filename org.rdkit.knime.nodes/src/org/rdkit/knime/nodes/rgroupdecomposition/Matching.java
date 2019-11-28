91e02b126273f79afdc1230466932267f026732c
Manuel Schwarze <manuel.schwarze@novartis.com>
2019-11-28 08:15:19 +0100

    New/rgroup decomposition (#72)
    * Added thread synchronization to avoid NullPointerException raise cond.
    
    Fixed line endings for Linux and encoding to UTF-8.
    
    * Unfinished changes for new RGroupDecomposition node
    
    * Undeprecated the old RDKit R-Group Decomposition node
    
    This will give users the chance to try both versions, old and new, at
    the same time, while the new version is still in beta state.
    
    * Some improvements in InputDataInfo handling
    
    Added new constructors to allow handover of a list of acceptable data
    value classes to be used as input instead of using an array, which
    caused issues when using generic data types.
    Better handling for adapter cells for SMILES, SDF and SMARTS.
    
    * Improved error handling for undefined enum selection parameter
    
    * Added new RDKit R-Group Decomposition node as beta version.
    
    This is based on RDKit functionality developed in 2018/2019. Eventually
    this will completely replace the old RDKit R-Group Decomposition node,
    which has been deprecated but can still be used in the moment.
    
    * Renamed old R-Group Decomposition tests
    
    * Added new temporary R-Group Decomposition tests
    
    * Delete old test duplicate
    
    * Updated copyright information
    
    * Bugfixes and added Remove Empty Rx Columns parameter
    
    * update expected results
    
    * Bugfix to avoid RDKit exceptions after processing failure
    
    * Updated R-Group Decomposition test workflow
    
    * update expected results
    
    * needs testing
    
    * update expected test results
    
    * update windows binary, remove old boost libs, update loader
    
    * update tests for windows
    
    * simplify RMSD tests to remove chances for roundoff error to bite us
    
    * remove a bogus workflowset.meta file
    
    * add target definition for KNIME 4.0
    
    * bump the binaries to 4.0
    
    * Added thread synchronization to avoid NullPointerException raise cond.
    
    Fixed line endings for Linux and encoding to UTF-8.
    
    * Unfinished changes for new RGroupDecomposition node
    
    * Undeprecated the old RDKit R-Group Decomposition node
    
    This will give users the chance to try both versions, old and new, at
    the same time, while the new version is still in beta state.
    
    * Some improvements in InputDataInfo handling
    
    Added new constructors to allow handover of a list of acceptable data
    value classes to be used as input instead of using an array, which
    caused issues when using generic data types.
    Better handling for adapter cells for SMILES, SDF and SMARTS.
    
    * Improved error handling for undefined enum selection parameter
    
    * Added new RDKit R-Group Decomposition node as beta version.
    
    This is based on RDKit functionality developed in 2018/2019. Eventually
    this will completely replace the old RDKit R-Group Decomposition node,
    which has been deprecated but can still be used in the moment.
    
    * Renamed old R-Group Decomposition tests
    
    * Added new temporary R-Group Decomposition tests
    
    * Delete old test duplicate
    
    * Updated copyright information
    
    * Bugfixes and added Remove Empty Rx Columns parameter
    
    * Bugfix to avoid RDKit exceptions after processing failure
    
    * Updated R-Group Decomposition test workflow
    
    * add a smaller test with clear known results

---
+++ org.rdkit.knime.nodes/src/org/rdkit/knime/nodes/rgroupdecomposition/Matching.java
@@ -0,0 +1,185 @@
+/*
+ * ------------------------------------------------------------------
+ * This source code, its documentation and all appendant files
+ * are protected by copyright law. All rights reserved.
+ *
+ * Copyright (C)2019
+ * Novartis Institutes for BioMedical Research
+ *
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ */
+package org.rdkit.knime.nodes.rgroupdecomposition;
+
+import org.RDKit.RGroupCoreAlignment;
+import org.RDKit.RGroupMatching;
+
+/**
+ * This enumeration defines different R Group matching strategies based on the 
+ * RDKit R Group Matching enumeration.
+ * Details: https://www.rdkit.org/docs/cppapi/RGroupDecomp_8h_source.html
+ * 
+ * @author Manuel Schwarze
+ */
+public enum Matching {
+
+	Greedy("Greedy", RGroupMatching.Greedy),
+	GreedyChunks("Greedy Chunks", RGroupMatching.GreedyChunks), 
+	Exhaustive("Exhaustive", RGroupMatching.Exhaustive); // Not useful for large datasets
+
+	//
+	// Members
+	//
+
+	/** Enumeration value of RDKit. */
+	private final RGroupMatching m_rdkitRGroupMatching;
+	
+	/** The name to be shown to the user. */
+	private final String m_strName;
+
+	//
+	// Constructors
+	//
+
+	/**
+	 * Creates a new R Group Core Matching enumeration value.
+	 * 
+	 * @param strName Name to be shown as string representation.
+	 */
+	private Matching(final String strName, final RGroupMatching rdkitRGroupMatching) {
+		m_rdkitRGroupMatching = rdkitRGroupMatching;
+		m_strName = strName;
+	}
+
+	/**
+	 * Returns the associated RDKit R Group Matching enumeration value.
+	 * @return
+	 */
+	public RGroupMatching getRDKitRGroupMatching() {
+		return m_rdkitRGroupMatching;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String toString() {
+		return m_strName;
+	}
+
+	//
+	// Static Methods
+	//
+
+	/**
+	 * Tries to determine the R Group Matching type based on the passed in string. First it
+	 * will try to determine it by assuming that the passed in string is the
+	 * name of the R Group Matching type ({@link #name()}. If this fails, it will compare the
+	 * string representation trying to find a match there ({@link #toString()}.
+	 * If none is found it will return null.
+	 */
+	public static Matching parseString(String str) {
+		Matching matching = null;
+
+		if (str != null) {
+			try {
+				matching = Matching.valueOf(str);
+			}
+			catch (final IllegalArgumentException exc) {
+				// Ignored here
+			}
+
+			if (matching == null) {
+				str = str.trim().toUpperCase();
+				for (final Matching matchingExisting : Matching.values()) {
+					if (str.equals(matchingExisting.toString().toUpperCase()) ||
+						str.equals(matchingExisting.getRDKitRGroupMatching().name().toUpperCase())) {
+						matching = matchingExisting;
+						break;
+					}
+				}
+			}
+		}
+
+		return matching;
+	}
+	
+	/**
+	 * Determines the matching Matching value for the specified RDKit based value
+	 * and returns null, if undefined.
+	 * 
+	 * @param value RDKit based value. Can be null to return null.
+	 * 
+	 * @return KNIME / Java based value. Null, if undefined.
+	 */
+	public static Matching getValue(RGroupCoreAlignment value) {
+		Matching result = null;
+		
+		if (value != null) {
+			for (Matching matching : values()) {
+				if (matching.getRDKitRGroupMatching().swigValue() == value.swigValue()) {
+					result = matching;
+				}
+			}
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * Determines the matching Matching value for the specified RDKit based SWIG value
+	 * and returns null, if undefined.
+	 * 
+	 * @param value RDKit based value. Can be null to return null.
+	 * 
+	 * @return KNIME / Java based value. Null, if undefined.
+	 */
+	public static Matching getValue(long value) {
+		Matching result = null;
+		
+		for (Matching matching : values()) {
+			if (matching.getRDKitRGroupMatching().swigValue() == value) {
+				result = matching;
+			}
+		}
+		
+		return result;
+	}
+}
---
+++ org.rdkit.knime.nodes/src/org/rdkit/knime/nodes/rgroupdecomposition/RDKitRGroupDecompositionNodeDialog.java
@@ -0,0 +1,327 @@
+/*
+ * ------------------------------------------------------------------
+ * This source code, its documentation and all appendant files
+ * are protected by copyright law. All rights reserved.
+ *
+ * Copyright (C) 2019
+ * Novartis Institutes for BioMedical Research
+ *
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ */
+package org.rdkit.knime.nodes.rgroupdecomposition;
+
+import java.awt.Dimension;
+
+import javax.swing.BorderFactory;
+
+import org.RDKit.RGroupDecompositionParameters;
+import org.knime.chem.types.SdfValue;
+import org.knime.chem.types.SmartsValue;
+import org.knime.chem.types.SmilesValue;
+import org.knime.core.node.NotConfigurableException;
+import org.knime.core.node.defaultnodesettings.DialogComponent;
+import org.knime.core.node.defaultnodesettings.DialogComponentBoolean;
+import org.knime.core.node.defaultnodesettings.DialogComponentLabel;
+import org.knime.core.node.defaultnodesettings.DialogComponentMultiLineString;
+import org.knime.core.node.defaultnodesettings.SettingsModelBoolean;
+import org.knime.core.node.defaultnodesettings.SettingsModelString;
+import org.knime.core.node.port.PortObjectSpec;
+import org.rdkit.knime.nodes.AbstractRDKitNodeSettingsPane;
+import org.rdkit.knime.types.RDKitMolValue;
+import org.rdkit.knime.util.DialogComponentColumnNameSelection;
+import org.rdkit.knime.util.DialogComponentEnumFilterPanel;
+import org.rdkit.knime.util.DialogComponentEnumSelection;
+import org.rdkit.knime.util.SettingsModelEnumeration;
+import org.rdkit.knime.util.SettingsModelEnumerationArray;
+
+/**
+ * <code>NodeDialog</code> for the "RDKitRGroups" Node.
+ * 
+ * @author Manuel Schwarze
+ */
+public class RDKitRGroupDecompositionNodeDialog extends AbstractRDKitNodeSettingsPane {
+	
+	//
+	// Constants
+	//
+	
+	/** Default RGroup Decomposition Parameters as defined by the RDKit. */	
+	public static final RGroupDecompositionParameters DEFAULT_RGROUP_DECOMPOSITION_PARAMETERS = 
+			new RGroupDecompositionParameters(); // Will not be cleaned up
+	
+	//
+	// Members
+	//
+	
+	/** Setting model component for the additional column selector. */
+	private final DialogComponent m_compCoreInputColumnName;
+
+	/** Setting model component for the cores input text field. */
+	private final DialogComponent m_compCoreInputTextField;
+	
+	/** Setting model component for the hint label to connect a second table. */
+	private final DialogComponent m_compHintToConnectSecondTable;
+	
+	//
+	// Constructor
+	//
+
+	/**
+	 * Create a new dialog pane with default components to configure an input column,
+	 * the name of a new column, which will contain the calculation results, an option
+	 * to tell, if the source column shall be removed from the result table.
+	 */
+	RDKitRGroupDecompositionNodeDialog() {
+		super.createNewGroup("Input Molecules");
+		super.addDialogComponent(new DialogComponentColumnNameSelection(
+				createInputColumnNameModel(), "RDKit Mol column: ", 0,
+				RDKitMolValue.class));
+		super.createNewGroup("Input Scaffolds");
+		super.addDialogComponent(m_compHintToConnectSecondTable = new DialogComponentLabel(
+				"<html><body>You may connect a second input table with molecules to be used as cores<br>or you specify one or more cores in the text field below.</body></html>"));
+		super.addDialogComponent(m_compCoreInputColumnName = new DialogComponentColumnNameSelection(
+				createCoresInputColumnNameModel(), "Core Input Column: ", 1, false, false, 
+				RDKitMolValue.class, SmilesValue.class, SmartsValue.class, SdfValue.class) {
+
+			/**
+			 * Hides or shows the optional components depending on
+			 * the existence of a second input table.
+			 */
+			@Override
+			protected void checkConfigurabilityBeforeLoad(
+					final PortObjectSpec[] specs)
+							throws NotConfigurableException {
+
+				final boolean bHasAdditionalInputTable =
+						RDKitRGroupDecompositionNodeModel.hasAdditionalInputTable(specs);
+
+				// Only check correctness of second input table if it is there
+				if (bHasAdditionalInputTable) {
+					super.checkConfigurabilityBeforeLoad(specs);
+				}
+
+				// Always show or hide proper components
+				updateVisibilityOfOptionalComponents(bHasAdditionalInputTable);
+			}
+		});
+		super.addDialogComponent(m_compCoreInputTextField = new DialogComponentMultiLineString(
+				createSmartsModel(), "Core SMARTS (separate by new line to specify multiple cores): ", 
+				false, 50, 5));
+		m_compCoreInputTextField.getComponentPanel().setBorder(BorderFactory.createEmptyBorder(10,  10,  10, 10));
+
+		
+		super.createNewGroup("Output Handling");
+		super.addDialogComponent(new DialogComponentBoolean(
+				createRemoveEmptyColumnsModel(), "Remove empty Rx columns"));
+		super.addDialogComponent(new DialogComponentBoolean(
+				createFailForNoMatchOptionModel(), "Fail if no cores are matching at all"));
+		
+		super.createNewTab("Advanced");
+		
+		DialogComponentEnumFilterPanel<Labels> dcLabels = 
+				new DialogComponentEnumFilterPanel<Labels>(createLabelsModel(), 
+						"Labels to recognize R-Groups in scaffolds:", null, false /* disallow no selection */); 
+		dcLabels.setIncludeTitle("Enabled");
+		dcLabels.setExcludeTitle("Disabled");
+		dcLabels.setSearchVisible(false);
+		dcLabels.getComponentPanel().setPreferredSize(new Dimension(450,170));
+		dcLabels.getComponentPanel().setMinimumSize(new Dimension(450,170));
+		dcLabels.getComponentPanel().setMaximumSize(new Dimension(450,170));
+		super.addDialogComponent(dcLabels);
+		
+		super.addDialogComponent(new DialogComponentEnumSelection<Matching>(createMatchingStrategyModel(), "Matching strategy:"));
+
+		DialogComponentEnumFilterPanel<Labeling> dcLabeling = 
+				new DialogComponentEnumFilterPanel<Labeling>(createLabelingModel(), 
+						"Labeling for R-Groups output:", null, true /* no selection is allowed */);
+		dcLabeling.setIncludeTitle("Enabled");
+		dcLabeling.setExcludeTitle("Disabled");
+		dcLabeling.setSearchVisible(false);
+		dcLabeling.setButtonVisible(DialogComponentEnumFilterPanel.BUTTON_ADD_ALL, false);
+		dcLabeling.setButtonVisible(DialogComponentEnumFilterPanel.BUTTON_REMOVE_ALL, false);
+		dcLabeling.getComponentPanel().setPreferredSize(new Dimension(450,120));
+		dcLabeling.getComponentPanel().setMinimumSize(new Dimension(450,120));
+		dcLabeling.getComponentPanel().setMaximumSize(new Dimension(450,120));
+		super.addDialogComponent(dcLabeling);
+
+		super.addDialogComponent(new DialogComponentEnumSelection<CoreAlignment>(createCoreAlignmentModel(), "Core alignment:"));
+		super.addDialogComponent(new DialogComponentBoolean(createMatchOnlyAtRGroupsModel(), "Match only at R-Groups"));
+		super.setHorizontalPlacement(true);
+		super.addDialogComponent(new DialogComponentBoolean(createRemoveHydrogenOnlyRGroupsModel(), "Remove hydrogen only R-Groups"));
+		super.addDialogComponent(new DialogComponentBoolean(createRemoveHydrogensPostMatchModel(), "Remove hydrogens post match"));
+	}
+
+	//
+	// Protected Methods
+	//
+	
+	/**
+	 * Show or hides additional input based settings based on availability of the
+	 * optional second input table.
+	 * 
+	 * @param bHasAdditionalInputTable
+	 */
+	protected void updateVisibilityOfOptionalComponents(final boolean bHasAdditionalInputTable) {
+		m_compHintToConnectSecondTable.getComponentPanel().setVisible(!bHasAdditionalInputTable);
+		m_compCoreInputColumnName.getComponentPanel().setVisible(bHasAdditionalInputTable);
+		m_compCoreInputTextField.getComponentPanel().setVisible(!bHasAdditionalInputTable);
+	}
+
+	//
+	// Static Methods
+	//
+
+	/**
+	 * Creates the settings model to be used for the input column.
+	 * 
+	 * @return Settings model for input column selection.
+	 */
+	static final SettingsModelString createInputColumnNameModel() {
+		return new SettingsModelString("input_column", null);
+	}
+
+	/**
+	 * Creates the settings model to be used for the cores input column
+	 * in the optional second ionput table.
+	 * 
+	 * @return Settings model for cores input column selection.
+	 */
+	static final SettingsModelString createCoresInputColumnNameModel() {
+		return new SettingsModelString("cores_input_column", null);
+	}
+
+	/**
+	 * Creates the settings model to be used as core SMARTS.
+	 * 
+	 * @return Settings model for the core SMARTS value.
+	 */
+	static final SettingsModelString createSmartsModel() {
+		return new SettingsModelString("smarts_value", "");
+	}
+
+	/**
+	 * Creates the settings model for the option to remove empty Rx columns.
+	 * 
+	 * @return Settings model for removing empty Rx columns.
+	 */
+	static final SettingsModelBoolean createRemoveEmptyColumnsModel() {
+		return new SettingsModelBoolean("remove_empty_columns", true);
+	}
+	
+	/**
+	 * Creates the settings model for the option to let the node fail, if there is match at all.
+	 * 
+	 * @return Settings model for the option to let the node fail, if there is match at all.
+	 */
+	static final SettingsModelBoolean createFailForNoMatchOptionModel() {
+		return new SettingsModelBoolean("fail_for_no_match", true);
+	}
+	
+	/**
+	 * Creates the settings model for the R-Group labels options.
+	 * 
+	 * @return Settings model for R-Group labels options.
+	 */
+	static final SettingsModelEnumerationArray<Labels> createLabelsModel() {
+		return new SettingsModelEnumerationArray<>(Labels.class, "labels", new Labels[] { Labels.AutoDetect });
+	}
+	
+	/**
+	 * Creates the settings model for the R-Group matching strategy option.
+	 * 
+	 * @return Settings model for R-Group matching strategy.
+	 */
+	static final SettingsModelEnumeration<Matching> createMatchingStrategyModel() {
+		Matching defaultValue = Matching.getValue(DEFAULT_RGROUP_DECOMPOSITION_PARAMETERS.getMatchingStrategy());
+		return new SettingsModelEnumeration<>(Matching.class, "matching_strategy", 
+				defaultValue == null ? Matching.GreedyChunks : defaultValue);
+	}
+	
+	/**
+	 * Creates the settings model for the R-Group labeling options.
+	 * 
+	 * @return Settings model for R-Group labeling options.
+	 */
+	static final SettingsModelEnumerationArray<Labeling> createLabelingModel() {
+		return new SettingsModelEnumerationArray<>(Labeling.class, "labeling", new Labeling[] { Labeling.AtomMap, Labeling.MDLRGroup});
+	}
+
+	/**
+	 * Creates the settings model for the R-Group core alignment option.
+	 * 
+	 * @return Settings model for R-Group core alignment.
+	 */
+	static final SettingsModelEnumeration<CoreAlignment> createCoreAlignmentModel() {
+		CoreAlignment defaultValue = CoreAlignment.getValue(DEFAULT_RGROUP_DECOMPOSITION_PARAMETERS.getAlignment());
+		return new SettingsModelEnumeration<>(CoreAlignment.class, "core_alignment", 
+				defaultValue == null ? CoreAlignment.MCS : defaultValue);
+	}
+
+	/**
+	 * Creates the settings model for the option to only match R-Groups.
+	 * 
+	 * @return Settings model for the option to only match R-Groups.
+	 */
+	static final SettingsModelBoolean createMatchOnlyAtRGroupsModel() {
+		return new SettingsModelBoolean("match_only_at_rgroups", 
+				DEFAULT_RGROUP_DECOMPOSITION_PARAMETERS.getOnlyMatchAtRGroups());
+	}
+
+	/**
+	 * Creates the settings model for the option to remove hydrogen only R-Groups.
+	 * 
+	 * @return Settings model for the option to remove hydrogen only R-Groups.
+	 */
+	static final SettingsModelBoolean createRemoveHydrogenOnlyRGroupsModel() {
+		return new SettingsModelBoolean("remove_hydrogen_only_rgroups", 
+				DEFAULT_RGROUP_DECOMPOSITION_PARAMETERS.getRemoveAllHydrogenRGroups());
+	}
+
+	/**
+	 * Creates the settings model for the option to remove hydrogens post match.
+	 * 
+	 * @return Settings model for the option to remove hydrogens post match.
+	 */
+	static final SettingsModelBoolean createRemoveHydrogensPostMatchModel() {
+		return new SettingsModelBoolean("remove_hydrogens_post_match", 
+				DEFAULT_RGROUP_DECOMPOSITION_PARAMETERS.getRemoveHydrogensPostMatch());
+	}
+}
